521e77152f3554dbdeaebddf5c1c050dc6158f78
stonexes <usamabinyousaf07@gmail.com>
2025-04-29 22:36:41 +0500

    Backend integration  Folder is in Project folder now

---
+++ Backend/expenses.js
@@ -0,0 +1,24 @@
+
+exports.expenses = [
+    {
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { 
+      title: "New TV", 
+      amount: 799.49, 
+      date: new Date(2021, 2, 12) 
+    },
+    {
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
